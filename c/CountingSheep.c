86f9105aa0a069c8e5c8fba98db76363eb5b34e8
Scott Demmert <73268848+s-jhay@users.noreply.github.com>
2026-06-28 12:14:47 -0400

    Update CountingSheep.c

--- c/CountingSheep.c
+++ c/CountingSheep.c
@@ -1,1 +1,15 @@
+#include <stdbool.h>
+#include <stddef.h>
 
+size_t count_sheep(const bool sheep[/* count */], size_t count)
+{
+  size_t present_sheep = 0;
+
+  for (size_t i = 0; i < count; i++) {
+    if (sheep[i] == true) {
+      present_sheep++;
+    }
+  }
+  
+  return present_sheep;
+}
