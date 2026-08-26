db0e50801044acaaecee99925fd35dfee00eeb5f
Scott Demmert <73268848+s-jhay@users.noreply.github.com>
2026-08-25 21:05:28 -0400

    Create CountByX.cs

---
+++ csharp/CountByX.cs
@@ -0,0 +1,16 @@
+using System;
+
+public static class Kata
+{
+  public static int[] CountBy(int x, int n)
+  {
+    int[] z = new int[n];
+    
+    for (int i = 0; i < n; i++) 
+    {
+      z[i] = (i + 1) * x;
+    }
+    
+    return z;
+  }
+}
