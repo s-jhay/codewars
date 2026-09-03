a09686ede96bbb1f48b3547288ad09545f0eeb25
Scott Demmert <73268848+s-jhay@users.noreply.github.com>
2026-09-02 21:18:32 -0400

    Create YouCantCodeUnderPressure.cs

---
+++ csharp/YouCantCodeUnderPressure.cs
@@ -0,0 +1,11 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+using System;
+
+public static class Kata
+{
+  public static int DoubleInteger(int n)
+  {
+    return n * 2;
+  }
+}
